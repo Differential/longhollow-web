db7ec90e0c1577b33e1a7d420a0e2e20a24e44bd
Russel Rajitha <russel.rajitha@differential.com>
2024-09-03 19:41:17 +0530

    Upgrade Node Engine to 20

--- components/ArticleLink/ArticleLink.js
+++ components/ArticleLink/ArticleLink.js
@@ -48,7 +48,7 @@ function ArticleLink({
           {splitString(description)}
         </Text>
         {url && urlText ? (
-          <Link legacyBehavior legacyBehavior href={url}>
+          <Link legacyBehavior href={url}>
             <a
               style={{
                 display: 'flex',
