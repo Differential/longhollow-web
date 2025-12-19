25e959357b9190f717e710a363fc26d6e9f8333f
Michael Neeley <micneeley14@gmail.com>
2025-12-19 15:26:39 -0500

    fix: update jest css mapper

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
