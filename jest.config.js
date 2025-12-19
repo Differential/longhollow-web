b48bc974bf1c1ad431e55c92a34f3d793f0e72cb
Michael Neeley <micneeley14@gmail.com>
2025-12-19 16:27:27 -0500

    chore: migrate jest config to next/jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   moduleDirectories: ['node_modules', '<rootDir>'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-  },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
