f512e9b0c9d9dec34e7840096e5edbff53b0fca5
Michael Neeley <micneeley14@gmail.com>
2025-12-19 16:23:00 -0500

    chore: migrate eslint config to flat

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    jest: true,
-  },
-  extends: ['next/core-web-vitals'],
-};
---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+const nextCoreWebVitals = require('eslint-config-next/core-web-vitals');
+
+module.exports = [
+  {
+    ignores: ['.next/**'],
+  },
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      '@next/next/no-html-link-for-pages': 'warn',
+      'react/display-name': 'warn',
+      'react/jsx-key': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'react-hooks/immutability': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+    },
+  },
+  {
+    files: [
+      '**/__tests__/**/*.{js,jsx,ts,tsx}',
+      '**/*.{test,spec}.{js,jsx,ts,tsx}',
+    ],
+    languageOptions: {
+      globals: {
+        afterAll: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        beforeEach: 'readonly',
+        describe: 'readonly',
+        expect: 'readonly',
+        it: 'readonly',
+        jest: 'readonly',
+      },
+    },
+  },
+];
