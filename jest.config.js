ba20501f789a4c5488c4fe44c8ffff85ae8a9fb8
Michael Neeley <micneeley14@gmail.com>
2025-12-19 15:32:22 -0500

    fix: stabilize jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
