4a3c97f54519269a7a64cd080fea64dfe6de8705
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2026-02-24 11:06:16 -0600

    Update /easter URL

--- next.config.js
+++ next.config.js
@@ -370,7 +370,7 @@ module.exports = {
       { source: '/local', destination: '/search?category=Service%20Opportunities&p=1', permanent: true },
       { source: '/easteroffering', destination: '/', permanent: true },
       { source: '/easteroffering', destination: '/spring-offering-2025', permanent: true },
-      { source: '/easter', destination: 'https://longholloweaster.com', permanent: true },
+      { source: '/easter', destination: '/easter-2026-1', permanent: true },
       { source: '/lhkids', destination: '/connect/kids', permanent: true },
       { source: '/christmas', destination: 'https://longhollowchristmas.com', permanent: true },
       { source: '/christmaseve', destination: '/christmas-eve-at-long-hollow-2025', permanent: true },
