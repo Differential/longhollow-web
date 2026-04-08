db1efda3cb20223778e4c8e467ace09dc7027523
anna-brackins <anna.brackins@longhollow.com>
2026-04-08 13:53:14 -0500

    Update /notes for sermon notes

--- next.config.js
+++ next.config.js
@@ -443,7 +443,7 @@ module.exports = {
       { source: '/events/light-up-the-pike', destination: 'https://lightupthepike.com', permanent: true },
       { source: '/groupserve', destination: 'https://my.longhollow.com/groupserve', permanent: true },
       { source: '/vendor', destination: 'https://longhollow.wufoo.com/forms/phu7t8c0lslify/', permanent: true },
-      { source: '/notes', destination: 'http://bible.com/events/49587555', permanent: true },
+      { source: '/notes', destination: 'http://bible.com/events/49591359', permanent: true },
       { source: '/kidscamp', destination: 'https://longhollow.com/kids-camp-2026', permanent: true },
       { source: '/studentcamp', destination: 'https://longhollow.com/student-camp-2026', permanent: true },
     ];
