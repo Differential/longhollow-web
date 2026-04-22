99c5a99a087280fb362eed00849f69c78d36c59b
anna-brackins <anna.brackins@longhollow.com>
2026-04-22 09:05:50 -0500

    Update /notes redirect destination URL

--- next.config.js
+++ next.config.js
@@ -443,7 +443,7 @@ module.exports = {
       { source: '/events/light-up-the-pike', destination: 'https://lightupthepike.com', permanent: true },
       { source: '/groupserve', destination: 'https://my.longhollow.com/groupserve', permanent: true },
       { source: '/vendor', destination: 'https://longhollow.wufoo.com/forms/phu7t8c0lslify/', permanent: true },
-      { source: '/notes', destination: 'http://bible.com/events/49595063', permanent: true },
+      { source: '/notes', destination: 'http://bible.com/events/49598068', permanent: true },
       { source: '/kidscamp', destination: 'https://longhollow.com/kids-camp-2026', permanent: true },
       { source: '/studentcamp', destination: 'https://longhollow.com/student-camp-2026', permanent: true },
       { source: '/thegospelofbeinghuman', destination: 'https://www.martysolomon.com', permanent: true },
