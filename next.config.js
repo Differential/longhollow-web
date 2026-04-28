64e4bd472e7fbd1512cd7af21dd3e034ed6d669d
anna-brackins <anna.brackins@longhollow.com>
2026-04-28 12:21:01 -0500

    Add redirect for /divorce-care-wednesday

--- next.config.js
+++ next.config.js
@@ -448,6 +448,7 @@ module.exports = {
       { source: '/studentcamp', destination: 'https://longhollow.com/student-camp-2026', permanent: true },
       { source: '/thegospelofbeinghuman', destination: 'https://www.martysolomon.com', permanent: true },
       { source: '/divorce-care-sunday', destination: 'https://my.longhollow.com/page/2823?GroupId=1161230&PageType=care', permanent: true },
+      { source: '/divorce-care-wednesday', destination: 'https://my.longhollow.com/page/2823?GroupId=1143173&PageType=care', permanent: true },
     ];
   }
 };
