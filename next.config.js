88b452b82fe9e4941d8571996ff6f948bbe4a06b
anna-brackins <anna.brackins@longhollow.com>
2026-04-28 12:26:11 -0500

    Add redirect for /healing-from-trauma

--- next.config.js
+++ next.config.js
@@ -450,6 +450,7 @@ module.exports = {
       { source: '/divorce-care-sunday', destination: 'https://my.longhollow.com/page/2823?GroupId=1161230&PageType=care', permanent: true },
       { source: '/divorce-care-wednesday', destination: 'https://my.longhollow.com/page/2823?GroupId=1143173&PageType=care', permanent: true },
       { source: '/graceformentalhealth', destination: 'https://my.longhollow.com/page/2823?GroupId=1161233&PageType=care', permanent: true },
+      { source: '/healingfromtrauma', destination: 'https://my.longhollow.com/page/2823?GroupId=1161237&PageType=care', permanent: true },
     ];
   }
 };
