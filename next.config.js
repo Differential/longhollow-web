cb0755ac929a277f487bda6d5d9803a0af10379e
anna-brackins <anna.brackins@longhollow.com>
2026-05-06 16:29:24 -0500

    Add redirect for '/teams' to volunteer page

--- next.config.js
+++ next.config.js
@@ -451,6 +451,7 @@ module.exports = {
       { source: '/divorce-care-wednesday', destination: 'https://my.longhollow.com/page/2823?GroupId=1143173&PageType=care', permanent: true },
       { source: '/graceformentalhealth', destination: 'https://my.longhollow.com/page/2823?GroupId=1161233&PageType=care', permanent: true },
       { source: '/healingfromtrauma', destination: 'https://my.longhollow.com/page/2823?GroupId=1161237&PageType=care', permanent: true },
+      { source: '/teams', destination: 'https://longhollow.com/next-steps/volunteer', permanent: true }, 
     ];
   }
 };
